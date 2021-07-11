639c8f61093a1d9be892c416e9b732d8b2363be5
jevillegasd-nyu <jevillegasd@gmail.com>
2021-07-11 20:36:06 +0400

    Script Upodates

--- Scripts/ref/display_results.m
+++ Scripts/ref/display_results.m
@@ -6,7 +6,7 @@ function tout = display_results(avg, sigma)
 
     metrics = {'ED';'LHD';'BHD'}; clear out;
     for j = 2:4 %metric
-        for i = 1:3 %device design
+        for i = 1:size(avg,1) %device design
             if avg(i,j)>1
                 s = sprintf('%2.1f\x00B1 %2.1f (%2.1f\x00B1 %2.1f) \t', ...
                 avg(i,j),sigma(i,j),avg(i,j+4),sigma(i,j+4));
@@ -21,6 +21,8 @@ function tout = display_results(avg, sigma)
         disp(' ');
     end
     
-    tout = cell2table( out, ...
-    'VariableNames',{'Metric' 'Design 1' 'Design 2' 'Design 3'});
+    if avg(i,j)>1
+        tout = cell2table( out, ...
+        'VariableNames',{'Metric' 'Design 1' 'Design 2' 'Design 3'});
+    end
 end
